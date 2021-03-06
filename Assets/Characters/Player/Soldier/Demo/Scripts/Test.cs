fd497958b3411d8d1d73b270ce864aeeedfd1ea1
Mark Ellis <mellis6464@floridapoly.edu>
2021-03-06 11:41:11 -0500

    Added art

---
+++ Assets/Characters/Player/Soldier/Demo/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Actions a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = this.GetComponent<Actions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Vertical") > 0)
+        {
+            a.Aiming();
+        }
+    }
+}
